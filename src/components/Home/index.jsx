38f532b4f6d71d888da899181c58320e88f1d880
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-03 17:26:37 -0300

    accedo al id

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
+  const [id, setId] = useState(null);
+
+  const handleChage = (event) => {
+    setId(event.target.value);
+  };
   return (
     <main>
       <h1>ChatApp</h1>
-      <input type="text" name="" id="" placeholder="Write an id" />
-      <button>Go to chat</button>
+      <input type="text" placeholder="Write an id" onChange={handleChage} />
+      <Link to={`/chat/${id}`}>
+        <button>Go to chat</button>
+      </Link>
     </main>
   );
 };
