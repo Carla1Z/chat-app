9f4b567b618578cedef958e080303279d39e9558
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-12 03:34:14 -0300

    stilos globos de conversacion

--- front/src/components/Message/index.jsx
+++ front/src/components/Message/index.jsx
@@ -4,10 +4,12 @@ import styles from "./styles.module.css";
 export const Message = ({ text, hour, sender = false }) => {
   return (
     <li className={styles.message}>
-      <div className={sender ? styles.sender : styles.receiver}>
-        <p>{text}</p>
-        <span>{hour}</span>
-      </div>
+        <div className={sender ? styles.sender : styles.receiver}>
+          <p>{text}</p>
+        </div>
+        <span className={sender ? styles.senderHour : styles.receiverHour}>
+          {hour}
+        </span>
     </li>
   );
 };
